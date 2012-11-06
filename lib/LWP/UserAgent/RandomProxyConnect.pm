6d6546b07ea773b744c95407dcccbf6c7453bdee
Héctor Valverde <hvalverde@uma.es>
2012-11-06 16:16:36 +0100

    First commit

---
+++ lib/LWP/UserAgent/RandomProxyConnect.pm
@@ -0,0 +1,111 @@
+package LWP::UserAgent::RandomProxyConnect;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+LWP::UserAgent::RandomProxyConnect - The great new LWP::UserAgent::RandomProxyConnect!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use LWP::UserAgent::RandomProxyConnect;
+
+    my $foo = LWP::UserAgent::RandomProxyConnect->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Hector Valverde, C<< <hvalverde at uma.es> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-lwp-useragent-randomproxyconnect at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=LWP-UserAgent-RandomProxyConnect>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc LWP::UserAgent::RandomProxyConnect
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=LWP-UserAgent-RandomProxyConnect>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/LWP-UserAgent-RandomProxyConnect>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/LWP-UserAgent-RandomProxyConnect>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/LWP-UserAgent-RandomProxyConnect/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Hector Valverde.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of LWP::UserAgent::RandomProxyConnect
