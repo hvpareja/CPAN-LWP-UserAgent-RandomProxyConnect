492d365cd8b4595f25c71c5ae42bcf2790a5767d
Héctor Valverde <hvalverde@uma.es>
2012-11-06 18:20:57 +0100

    Class structure drawn

--- lib/LWP/UserAgent/RandomProxyConnect.pm
+++ lib/LWP/UserAgent/RandomProxyConnect.pm
@@ -1,53 +1,251 @@
 package LWP::UserAgent::RandomProxyConnect;
+use base( "LWP::UserAgent" );
 
 use 5.006;
 use strict;
-use warnings FATAL => 'all';
+use warnings;
+our $AUTOLOAD;
+use Carp;
 
 =head1 NAME
 
-LWP::UserAgent::RandomProxyConnect - The great new LWP::UserAgent::RandomProxyConnect!
+LWP::UserAgent::RandomProxyConnect - A LWP::UserAgent extension for becoming an omnipresent client.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.10
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.10';
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This Object does exactly the same than the L<LWP::UserAgent> class with the a
+new feature: it can make each HTTP request throw a different proxy each time.
+Also, a few methods improve the proxy list management, and makes the iterative
+connections faster.
 
-Perhaps a little code snippet.
+=head1 CONSTRUCTOR
 
-    use LWP::UserAgent::RandomProxyConnect;
+When this class is invoked as:
 
-    my $foo = LWP::UserAgent::RandomProxyConnect->new();
-    ...
+    my $obj = LWP::UserAgent::RandomProxyConnect->new
+    
+several test will be made. First, the class must find a valid file with a proxy
+list, if not, this object will stop. This file must be placed in the environmental
+variable $ENV{PROXY_LIST}.
 
-=head1 EXPORT
+However, the class can be invoked as:
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    my $obj = LWP::UserAgent::RandomProxyConnect->new(-proxy_list => $proxy_file_path)
+    
+the created object will search the file at the specified path.
 
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
+Furthermore, whatever the method you use to invoke the class, the object will
+stop if the specified file doest not exists, is not readable or there is no proxy
+found into it.
 
 =cut
 
-sub function1 {
+sub new{
+    
+    my ($class, %arg) = @_;
+    my $self = bless {}, $class;
+    
+    # The following block is not really needed, but it is
+    # paste here for teaching reasons.
+    # The shorter way to set the unique attribute that the class
+    # need is like so:
+    #
+    # if($arg{-proxy-list}){
+    #   $self->set_proxy_list($arg{-proxy-list});      
+    # }
+    #
+    
+    # See below to see all attributes
+    foreach my $attribute ($self->_all_attributes()){
+        # E.g. attribute = "_name", argument = "name"
+        my ($argument) = ($attribute =~ /^_(.*)/);
+        # If explicitly given
+        if(exists $arg{$argument}){
+            $self->{$attribute} = $arg{$argument};
+        }
+        # Set to default
+        else{
+            $self->{$attribute} = $self->_attribute_default($attribute);            
+        }
+    }
+    
+    # Let's load a new "current_proxy". By this way, if there are any errors
+    # the object will stop.
+    $self->_renove_proxy;
+    
+    return $self;
+    
 }
 
-=head2 function2
+=head1 THE EXTENDED METHOD
+
+=head2 request
+
+This method is exactly the same than LWP::UserAgent->request L<LWP::UserAgent>
+with the implemented proxy-change in each request. It obiously make the connection
+slowler. NOTICE: Only http and https protocols are allowed.
 
 =cut
 
-sub function2 {
+sub request
+{
+    
+    my($self, $request, $arg, $size, $previous) = @_;
+    
+    # Get a new proxy from the list
+    $self->_renove_proxy;
+
+    # Get the proxy
+    my $new_proxy = $self->get_current_proxy;
+    # Set the proxy in the user agent
+    $self->proxy(['http','https'],$new_proxy);
+    
+    # Set the "last proxy used" value
+    $self->set_last_proxy($new_proxy);
+    
+    # Make the request
+    my $response = $self->SUPER->request($request,$arg,$size,$previous);
+    
+    # Return exactly the same than LWP::UserAgeng->request($request) method
+    return ($response);
+    
 }
+
+
+=head1 ATTRIBUTES
+
+=head2 proxy_list
+
+The C<proxy_list> attribute contains the string with the proxy list file path.
+The accessor method:
+
+    my $proxy_list = $obj->get_proxy_list;
+    
+returns such string.
+
+Also it can be set by the mutator method:
+
+    $obj->set_proxy_list($new_proxy_list_value);
+
+=head2 allowed_protocols
+
+=cut
+{
+    # A list of all attributes wiht default values and read/write/required properties
+    my %_attribute_properties = (
+        _proxy_list        => [$ENV{"PROXY_LIST"}, "read.write"], # The path to the proxy list file
+        _allowed_protocols => [['http','https'], "read.write"],
+        _current_proxy     => ["????:??", "read.write"],
+        _last_proxy        => ["????:??", "read.write"]
+    );
+    
+    # The list of all attributes
+    sub _all_attributes {
+        keys %_attribute_properties;
+    }
+    
+    # Return the default value for a given attribute
+    sub _attribute_default{
+        my ($self,$attribute) = @_;
+        $_attribute_properties{$attribute}[0];
+    }
+    
+}
+
+
+
+
+=head1 METHODS FOR HANDLING THE PROXY LIST
+
+=head2 _renove_proxy
+
+This function returns a new random proxy from the list. This return value
+is a string with the format: <proxyUrlorIP>:<port>. This is just a query
+for a single request.
+
+=cut
+
+sub _renove_proxy {
+    
+    my ($self) = @_;
+    
+    if(1){
+        my $obj_name = ref($self);
+        #croak("The object ".$obj_name." could not load any proxy at ".$self->get_proxy_list."\n");
+    }
+    
+    return 0;
+    
+}
+
+
+
+
+
+
+#
+# The AUTOLOAD method to get/set the class attributes
+# sub get_attribute {...}
+# sub set_attribute {...}
+sub AUTOLOAD{
+    
+    my ($self,$newvalue) = @_;
+    
+    my ($operation,$attribute) = ($AUTOLOAD =~ /(get|set)(_\w+)$/);
+    
+    # Is this a legal method name?
+    unless($operation && $attribute){ croak "Method name $AUTOLOAD is not the recogniced form (get|set)_attribute\n"; }
+    unless(exists $self->{$attribute}){ croak "No such attribute '$attribute' exists in the class ", ref($self); }
+    
+    # Turn off strict references to enagle magic AUTOLOAD speedup
+    no strict 'refs';
+    
+    # AUTOLOAD Accessors
+    if($operation eq 'get'){
+        # Define subroutine
+        *{$AUTOLOAD} = sub { shift->{$attribute} };
+    
+    # AUTOLOAD Mutators
+    }elsif($operation eq 'set'){
+        # Define subroutine ...
+        *{$AUTOLOAD} = sub { shift->{$attribute} = shift; };
+        # ... and set the new attribute value.
+        $self->{$attribute} = $newvalue;
+    }
+    
+    # Turn strict references back on
+    use strict 'refs';
+    
+    # Return the attribute value
+    return $self->{$attribute};
+    
+}
+
+sub DESTROY{
+    my $self = @_;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 =head1 AUTHOR
 
@@ -58,9 +256,6 @@ Hector Valverde, C<< <hvalverde at uma.es> >>
 Please report any bugs or feature requests to C<bug-lwp-useragent-randomproxyconnect at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=LWP-UserAgent-RandomProxyConnect>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
@@ -94,6 +289,8 @@ L<http://search.cpan.org/dist/LWP-UserAgent-RandomProxyConnect/>
 
 =head1 ACKNOWLEDGEMENTS
 
+I thank the University of Malaga for being so incompetent and make me prove it. Obiously,
+I am being ironic.
 
 =head1 LICENSE AND COPYRIGHT
 
